7a7ceb46449256b39009788394846f46ba87636d
이승형 <102710821+RHPLUSSEUNG@users.noreply.github.com>
2024-03-14 16:05:03 +0900

    feat: Dialogue 편집 에디터 구현
    대화 시스템에 필요한 대화(SO)들을 노드화하여 시스템 관리에 용이한 에디터 구현 중 / 노드 생성, 베지어 커브를 이용한 노드 연결 구현

---
+++ Assets/02_Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+
+[CreateAssetMenu(fileName = "New Dialogue", menuName = "Dialogue", order = 0)]
+public class Dialogue : ScriptableObject
+{
+    [SerializeField] List<DialogueNode> nodes = new List<DialogueNode>();
+
+    Dictionary<string, DialogueNode> nodeLookUp = new Dictionary<string, DialogueNode>();
+
+#if UNITY_EDITOR
+    private void Awake()
+    {
+        if(nodes.Count == 0)
+        {
+            DialogueNode rootNode = new DialogueNode();
+            rootNode.uniqueID = Guid.NewGuid().ToString();
+            nodes.Add(rootNode);
+        }
+    }
+#endif
+    private void OnValidate()
+    {
+        nodeLookUp.Clear();
+
+        foreach(DialogueNode node in nodes) 
+        {
+            nodeLookUp[node.uniqueID] = node;
+        }
+    }
+
+    public void CreateRootNode()
+    {
+        if (nodeLookUp.Count == 0)
+        {
+            nodes.Add(new DialogueNode()
+            {
+                uniqueID = Guid.NewGuid().ToString()
+            });
+        }
+    }
+
+    public void CreateNode(DialogueNode parent)
+    {
+        DialogueNode newNode = new DialogueNode();
+        newNode.uniqueID = Guid.NewGuid().ToString();
+        parent.children.Add(newNode.uniqueID);
+        nodes.Add(newNode);
+        OnValidate();
+    }
+
+    public IEnumerable<DialogueNode> GetAllNodes()
+    {
+        return nodes;
+    }
+
+    public DialogueNode GetRootNode()
+    {
+        return nodes[0];
+    }
+
+    public IEnumerable<DialogueNode> GetAllChildren(DialogueNode parentNode)
+    {
+        foreach (string childID in parentNode.children)
+        {
+            if (nodeLookUp.ContainsKey(childID))
+            {
+                yield return nodeLookUp[childID];
+            }
+        }
+
+    }
+
+    public class DialogueModificationProcessor : AssetPostprocessor //NMC
+    {
+        private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+        {
+            foreach (string importedAsset in importedAssets)
+            {
+                Dialogue dialogue = AssetDatabase.LoadAssetAtPath(importedAsset, typeof(Dialogue)) as Dialogue;
+
+                if (dialogue != null)
+                    dialogue.CreateRootNode();
+            }
+        }
+    }
+}
+
+
---
+++ Assets/02_Scripts/Dialogue/Editor/DialogueEditor.cs
@@ -0,0 +1,156 @@
+using NUnit.Framework.Constraints;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.MPE;
+using UnityEngine;
+
+
+public class DialogueEditor : EditorWindow
+{
+    Dialogue selectedDialogue = null;
+    [NonSerialized] GUIStyle nodeStyle;
+    [NonSerialized] DialogueNode draggingNode = null;
+    [NonSerialized] Vector2 draggingOffset;
+    [NonSerialized] DialogueNode creatingNode = null;
+
+    [MenuItem("Window/Dialogue Editor")] //Callback
+    public static void ShowEditorWindow()
+    {
+        GetWindow(typeof(DialogueEditor), false, "Dialogue Editor");
+    }
+
+    [OnOpenAssetAttribute(1)]
+    public static bool OpenDialogue(int instanceID, int line)
+    {
+        Dialogue dialogue = EditorUtility.InstanceIDToObject(instanceID) as Dialogue; //Ä³½ºÆÃ
+
+        if (dialogue != null)
+        {
+            ShowEditorWindow();
+            return true;
+        }
+
+        return false;
+    }
+
+    private void OnEnable()
+    {
+        Selection.selectionChanged += OnSelectionChanged;
+        nodeStyle = new GUIStyle();
+        nodeStyle.normal.background = EditorGUIUtility.Load("node0") as Texture2D;
+        nodeStyle.normal.textColor = Color.white;
+        nodeStyle.padding = new RectOffset(20, 20, 20, 20);
+        nodeStyle.border = new RectOffset(12, 12, 12, 12);
+    }
+
+    private void OnSelectionChanged()
+    {
+        Dialogue newDialogue = Selection.activeObject as Dialogue;
+
+        if (newDialogue != null)
+        {
+            selectedDialogue = newDialogue;
+            Repaint();
+        }
+    }
+
+    private void OnGUI()
+    {
+        if (selectedDialogue == null)
+        {
+            EditorGUILayout.LabelField("No Dialogue Selected.");
+        }
+        else
+        {
+            ProcessEvents();
+
+            foreach (DialogueNode node in selectedDialogue.GetAllNodes())
+            {
+                DrawConnections(node);
+            }
+            foreach (DialogueNode node in selectedDialogue.GetAllNodes())
+            {
+                DrawNode(node);
+            }
+        }
+    }
+
+    void ProcessEvents()
+    {
+        if (Event.current.type == EventType.MouseDown && draggingNode == null)
+        {
+            draggingNode = GetNodeAtPoint(Event.current.mousePosition);
+
+            if (draggingNode != null)
+            {
+                draggingOffset = draggingNode.rect.position - Event.current.mousePosition;
+            }
+        }
+        else if (Event.current.type == EventType.MouseDrag && draggingNode != null)
+        {
+            Undo.RecordObject(selectedDialogue, "Move Dialogue Node");
+            draggingNode.rect.position = Event.current.mousePosition + draggingOffset;
+            GUI.changed = true;
+        }
+        else if (Event.current.type == EventType.MouseUp && draggingNode != null)
+        {
+            draggingNode = null;
+        }
+        if (creatingNode != null)
+        {
+            Undo.RecordObject(selectedDialogue, "Added Dialogue Node");
+            selectedDialogue.CreateNode(creatingNode);
+            creatingNode = null;
+        }
+    }
+
+    void DrawNode(DialogueNode node)
+    {
+        GUILayout.BeginArea(node.rect, nodeStyle);
+        EditorGUI.BeginChangeCheck();
+        string newText = EditorGUILayout.TextField(node.text);
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(selectedDialogue, "Update Dialogue Text");
+            node.text = newText;
+        }
+        if (GUILayout.Button("+"))
+        {
+            creatingNode = node;
+        }
+        GUILayout.EndArea();
+    }
+
+    void DrawConnections(DialogueNode node)
+    {
+        Vector3 startPosition = new Vector2(node.rect.xMax, node.rect.center.y);
+
+        foreach (DialogueNode childNode in selectedDialogue.GetAllChildren(node))
+        {
+            Vector3 endPosition = new Vector2(childNode.rect.xMin, childNode.rect.center.y);
+            Vector3 controlPointOffset = endPosition - startPosition;
+            controlPointOffset.y = 0;
+            controlPointOffset.x *= 0.8f;
+            Handles.DrawBezier(startPosition, endPosition, startPosition + controlPointOffset, endPosition - controlPointOffset, Color.yellow, null, 4f);
+        }
+    }
+
+    DialogueNode GetNodeAtPoint(Vector2 point)
+    {
+        DialogueNode foundNode = null;
+
+        foreach (DialogueNode node in selectedDialogue.GetAllNodes())
+        {
+            if (node.rect.Contains(point))
+            {
+                foundNode = node;
+            }
+        }
+
+        return foundNode;
+    }
+}
