6440bbbc14d94eeb28f16c79b02e619f21502d6d
박주현 <yeochi1201@gmail.com>
2024-03-24 21:31:28 +0900

    클라이언트 기본 스크립트

---
+++ Assets/02_Scripts/Network/Packet/PacketHandler.cs
@@ -0,0 +1,26 @@
+﻿using DummyClient;
+using ServerCore;
+using System;
+
+
+class PacketHandler
+{
+    public static void PlayerInfoReqHandler(PacketSession session, IPacket packet)
+    {
+        PlayerInfoReq p = packet as PlayerInfoReq;
+        Console.WriteLine($"PlayerInfoReq : {p.playerId} {p.playerName}");
+
+        foreach(PlayerInfoReq.Skill skill in p.skills)
+        {
+            Console.WriteLine($"Skill : {skill.skillId} {skill.duration} {skill.level}");
+        }
+    }
+
+    public static void S_ChatHandler(PacketSession session, IPacket packet)
+    {
+        S_Chat chatPacket = packet as S_Chat;
+        ServerSession serverSession = session as ServerSession;
+
+        //Console.WriteLine($"{chatPacket.playerId} : {chatPacket.chat}");
+    }
+}
---
+++ Assets/02_Scripts/Network/ServerCore/SendBuffer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ServerCore
+{
+    public class SendBufferHelper
+    {
+        public static ThreadLocal<SendBuffer> CurrentBuffer = new ThreadLocal<SendBuffer>(() => { return null; });
+        //max data size
+        public static int ChunkSize { get; set; } = 65535*100;
+        //open segment
+        public static ArraySegment<byte> Open (int reserveSize)
+        {
+            if(CurrentBuffer.Value == null)
+            {
+                CurrentBuffer.Value = new SendBuffer(ChunkSize);
+            }
+            //buffer size lack
+            if(CurrentBuffer.Value.FreeSize < reserveSize)
+            {
+                CurrentBuffer.Value = new SendBuffer(ChunkSize);
+            }
+            //open buffer
+            return CurrentBuffer.Value.Open(reserveSize);
+        }
+
+        public static ArraySegment<byte> Close(int usedSize)
+        {
+            //close buffer
+            return CurrentBuffer.Value.Close(usedSize);
+        }
+    }
+
+    public class SendBuffer
+    {
+        byte[] _buffer;
+        int _usedSize = 0;
+
+        public int FreeSize { get { return _buffer.Length - _usedSize; } }
+
+        public SendBuffer(int chunkSize)
+        {
+            _buffer = new byte[chunkSize];
+        }
+
+        public ArraySegment<byte> Open(int reserveSize)
+        {
+            //buffer size lack
+            if(reserveSize > FreeSize)
+            {
+                return new ArraySegment<byte>();
+            }
+            return new ArraySegment<byte>(_buffer, _usedSize, reserveSize);
+        }
+        public ArraySegment<byte> Close(int usedSize)
+        {
+            ArraySegment<byte> segment = new ArraySegment<byte>(_buffer, _usedSize, usedSize);
+            _usedSize += usedSize;
+
+            return segment;
+        }
+    }
+}
