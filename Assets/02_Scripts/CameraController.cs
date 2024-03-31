ff3e9cf58aefe18d589a2b385c557e55aa475201
REofCC <buzz0131@naver.com>
2024-03-31 19:28:18 +0900

    feat : 플레이어와 카메라 사이의 오브젝트를 투명화 하는 기능 추가
    투명화 기능을 넣을 오브젝트에 스크립트 추가 해야 적용
    씬에서 Gizmo를 통해 카메라와 플레이어 사이의 직선 확인 가능

--- Assets/02_Scripts/CameraController.cs
+++ Assets/02_Scripts/CameraController.cs
@@ -1,9 +1,10 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     [SerializeField]
-    GameObject Target;
+    GameObject target;
 
     [SerializeField]
     float offsetX = 0.0f;
@@ -16,7 +17,7 @@ public class CameraController : MonoBehaviour
     [SerializeField]
     float cameraSpeed = 10.0f;
 
-    private Vector3 targetPos;
+    private Vector3 setPos;
 
     private void Awake()
     {
@@ -24,12 +25,35 @@ public class CameraController : MonoBehaviour
     }
     void FixedUpdate()
     {
-        targetPos = new Vector3(
-            Target.transform.position.x + offsetX,
-            Target.transform.position.y + offsetY,
-            Target.transform.position.z + offsetZ
+        setPos = new Vector3(
+            target.transform.position.x + offsetX,
+            target.transform.position.y + offsetY,
+            target.transform.position.z + offsetZ
             );
 
-        transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * cameraSpeed);
+        transform.position = Vector3.Lerp(transform.position, setPos, Time.deltaTime * cameraSpeed);
+    }
+    void LateUpdate()
+    {
+        Vector3 direction = (target.transform.position - transform.position).normalized;
+        float distance = Vector3.Distance(transform.position, target.transform.position);
+        RaycastHit[] hits = Physics.RaycastAll(transform.position, direction, distance,
+                            1 << LayerMask.NameToLayer("EnvironmentObject"));
+
+        for (int i = 0; i < hits.Length; i++)
+        {
+            EnvironmentObject[] obj = hits[i].transform.GetComponentsInChildren<EnvironmentObject>();
+
+            for (int j = 0; j < obj.Length; j++)
+            {
+                obj[j].Transparent();
+            }
+        }
+        Debug.DrawRay(transform.position, direction* distance, Color.red);
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(this.transform.position, (target.transform.position - transform.position).normalized * Vector3.Distance(transform.position, target.transform.position));
     }
 }
---
+++ Assets/02_Scripts/EnvironmentObject.cs
@@ -0,0 +1,170 @@
+using System.Collections;
+using UnityEngine;
+
+public class EnvironmentObject : MonoBehaviour
+{
+    public bool isTransparent = false;
+
+    private MeshRenderer[] renderers;
+    private WaitForSeconds delay = new WaitForSeconds(0.001f);
+    private WaitForSeconds resetDelay = new WaitForSeconds(0.005f);
+
+    [SerializeField]
+    private const float THRESHOLD_ALPHA = 0.25f;
+    [SerializeField]
+    private const float THRESHOLD_MAX_TIMER = 0.3f;
+
+    private bool isReseting = false;
+    private float timer = 0f;
+    private Coroutine timeCheckCoroutine;
+    private Coroutine resetCoroutine;
+    private Coroutine startTransparentCoroutine;
+
+
+    void Awake()
+    {
+        renderers = GetComponentsInChildren<MeshRenderer>();
+    }
+
+    public void Transparent()
+    {
+        if (isTransparent)
+        {
+            timer = 0f;
+            return;
+        }
+
+        if (resetCoroutine != null && isReseting)
+        {
+            isReseting = false;
+            isTransparent = false;
+            StopCoroutine(resetCoroutine);
+        }
+
+        SetMaterialTransparent();
+        isTransparent = true;
+        startTransparentCoroutine = StartCoroutine(TransparentCoroutine());
+    }
+
+    private void SetMaterialRenderingMode(Material material, float mode, int renderQueue)
+    {
+        material.SetFloat("_Mode", mode);
+        material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+        material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+        material.SetInt("ZWrite", 0);
+        material.DisableKeyword("_ALPHATEST_ON");
+        material.EnableKeyword("_ALPHABLEND_ON");
+        material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+        material.renderQueue = renderQueue;
+    }
+
+    private void SetMaterialTransparent()
+    {
+        for (int i = 0; i < renderers.Length; i++)
+        {
+            foreach (Material material in renderers[i].materials)
+            {
+                SetMaterialRenderingMode(material, 3f, 3000);
+            }
+        }
+    }
+
+    private void SetMaterialOpaque()
+    {
+        for (int i = 0; i < renderers.Length; i++)
+        {
+            foreach (Material material in renderers[i].materials)
+            {
+                SetMaterialRenderingMode(material, 0f, -1);
+            }
+        }
+    }
+
+    public void ResetOriginalTransparent()
+    {
+        SetMaterialOpaque();
+        resetCoroutine = StartCoroutine(ResetOriginalTransparentCoroutine());
+    }
+
+    private IEnumerator TransparentCoroutine()
+    {
+        while (true)
+        {
+            bool isComplete = true;
+
+            for (int i = 0; i < renderers.Length; i++)
+            {
+                if (renderers[i].material.color.a > THRESHOLD_ALPHA)
+                    isComplete = false;
+
+                Color color = renderers[i].material.color;
+                color.a -= Time.deltaTime;
+                renderers[i].material.color = color;
+            }
+
+            if (isComplete)
+            {
+                CheckTimer();
+                break;
+            }
+
+            yield return delay;
+        }
+    }
+
+    private IEnumerator ResetOriginalTransparentCoroutine()
+    {
+        isTransparent = false;
+
+        while (true)
+        {
+            bool isComplete = true;
+
+            for (int i = 0; i < renderers.Length; i++)
+            {
+                if (renderers[i].material.color.a < 1f)
+                    isComplete = false;
+
+                Color color = renderers[i].material.color;
+                color.a += Time.deltaTime;
+                renderers[i].material.color = color;
+            }
+
+            if (isComplete)
+            {
+                isReseting = false;
+                break;
+            }
+
+            yield return resetDelay;
+        }
+    }
+
+    public void CheckTimer()
+    {
+        if (timeCheckCoroutine != null)
+        {
+            StopCoroutine(timeCheckCoroutine);
+        }
+        timeCheckCoroutine = StartCoroutine(CheckTimerCouroutine());
+    }
+
+    private IEnumerator CheckTimerCouroutine()
+    {
+        timer = 0f;
+
+        while (true)
+        {
+            timer += Time.deltaTime;
+
+            if (timer > THRESHOLD_MAX_TIMER)
+            {
+                isReseting = true;
+                ResetOriginalTransparent();
+                break;
+            }
+
+            yield return null;
+        }
+    }
+}
