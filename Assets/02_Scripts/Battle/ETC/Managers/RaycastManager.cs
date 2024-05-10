861fe03d8d572c707cd31290edd8d86162e2e48f
박주현 <yeochi1201@gmail.com>
2024-05-10 19:47:37 +0900

    feat / Map Connect

--- Assets/02_Scripts/Battle/ETC/Managers/RaycastManager.cs
+++ Assets/02_Scripts/Battle/ETC/Managers/RaycastManager.cs
@@ -6,36 +6,41 @@ public class RaycastManager
     public void OnUpdate()
     {
         if (Input.GetMouseButtonDown(0))
-        {   
+        {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            if (Physics.Raycast(ray, out hit) && hit.transform.gameObject.name == "CombatMap(Clone)")
+            
+            if (Physics.Raycast(ray, out hit))
             {
-                Debug.Log(hit.collider.gameObject.name);
-            }
-            //while battle
-
-            if (Physics.Raycast(ray, out hit) && Managers.Battle.battleState == BattleState.PlayerTurn)
-            {
-                //using skill
-                if (Managers.PlayerButton.state == ButtonState.Skill)
-                {
-                    Managers.PlayerButton.player.GetComponent<CharacterBattle>().UseSkill(Managers.PlayerButton.GetSkill(), hit.collider.gameObject);
-                }
-                else if (hit.transform.gameObject.CompareTag("Character"))
+                if (hit.transform.gameObject.CompareTag("Character"))
                 {
                     Debug.Log("Character Selected : " + hit.collider.gameObject.name);
-                    //TODO UI
+                    //TODO Charcter Info UI
                 }
                 else if (hit.transform.gameObject.CompareTag("Monster"))
                 {
                     Debug.Log("Monster Selected");
-                    //TODO UI
+                    //TODO Monster Info UI
                 }
+                //Player Turn
+                if (Managers.Battle.battleState == BattleState.PlayerTurn)
+                {
+                    if (Managers.PlayerButton.state == ButtonState.Skill)
+                    {
+                        Managers.PlayerButton.player.GetComponent<CharacterBattle>().UseSkill(Managers.PlayerButton.GetSkill(), hit.collider.gameObject);
+                    }
+                    else if(Managers.PlayerButton.state == ButtonState.Idle && hit.collider.gameObject.name == "CombatMap(Clone)")
+                    {
+                        //player ¿Ãµø
+                        Managers.PlayerButton.player.transform.position = hit.collider.transform.position;
+                    }
+                    
+                }
+                //Battle Setting
                 else if (Managers.PlayerButton.state == ButtonState.PlayerSet && hit.transform.gameObject.name == "CombatMap(Clone)")
                 {
                     Managers.PlayerButton.SetPosition(hit.collider.gameObject);
-                    //TODO Player Move
+
                 }
             }
         }
