d1327126e92ecabbd0ff2552fcf79a0a3c5a5026
LeeJiHun <102594788+JiHoon-Lee-Tony@users.noreply.github.com>
2024-05-19 21:29:39 +0900

    chore/ delete Using

--- Assets/02_Scripts/PM_UI/UI/UI_Event.cs
+++ Assets/02_Scripts/PM_UI/UI/UI_Event.cs
@@ -1,9 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 using UnityEngine.EventSystems;
-using System.Threading;
 using System;
 
 public class UI_Event : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler, IDragHandler
