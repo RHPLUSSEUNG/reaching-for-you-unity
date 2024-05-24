c9ec75a3da62eee28e01d58101e87e828615608b
이승형 <102710821+RHPLUSSEUNG@users.noreply.github.com>
2024-05-24 16:58:15 +0900

    feat: 오버레이 UI 창 전환 기능
    사이드 탭 버튼을 클릭시 해당 창으로 전환하는 기능 구현

--- Assets/02_Scripts/OverlayUI/OverlayUI.cs
+++ Assets/02_Scripts/OverlayUI/OverlayUI.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OverlayUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] Button[] overlayTabButtons;
+    [SerializeField] Button exitButton;
+
+    [SerializeField] GameObject[] overlayWindows;
+
+    private void Awake()
+    {        
+        for(int i = 0; i < overlayTabButtons.Length; i++)
+        {
+            int windowNumber = i;
+            overlayTabButtons[i].onClick.AddListener(() => OverlayTab(windowNumber));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        overlayTabButtons[0].GetComponent<Image>().enabled = false;
+        overlayWindows[0].SetActive(true);
+    }
+
+    public void OverlayTab(int windowNumber)
+    {
+        for(int i = 0; i < overlayWindows.Length; i++) 
+        {
+            if(i == windowNumber)
+            {
+                overlayTabButtons[i].GetComponent<Image>().enabled = false;
+                overlayTabButtons[i].enabled = false;
+                overlayWindows[i].SetActive(true);
+            }
+            else
+            {
+                overlayTabButtons[i].GetComponent<Image>().enabled = true;
+                overlayTabButtons[i].enabled = true;
+                overlayWindows[i].SetActive(false);
+            }              
+        }        
     }
 }
