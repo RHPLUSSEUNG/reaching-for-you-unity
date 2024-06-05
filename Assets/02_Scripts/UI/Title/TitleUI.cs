df2e4c79e6022e16537229d771109c9a17bad3b5
이승형 <102710821+RHPLUSSEUNG@users.noreply.github.com>
2024-06-05 20:53:01 +0900

    feat: Title UI 기능 구현
    씬 전환과 Title UI 구현, 빌드 셋팅 변경

---
+++ Assets/02_Scripts/UI/Title/TitleUI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleUI : MonoBehaviour
+{
+    [SerializeField] Button newGameButton;
+    [SerializeField] Button loadButton;
+    [SerializeField] Button optionButton;
+    [SerializeField] Button exitButton;
+
+    private void Awake()
+    {
+        newGameButton.onClick.AddListener(()=>SceneChanger.Instance.ChangeScene(SceneType.AM));
+        exitButton.onClick.AddListener(()=>Application.Quit());
+    }    
+}
