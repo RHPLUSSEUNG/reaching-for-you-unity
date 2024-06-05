863a6b38a70ac9f2a2c0011a4ee02d43741db0b2
이승형 <102710821+RHPLUSSEUNG@users.noreply.github.com>
2024-06-05 20:52:20 +0900

    feat: SceneChanger 스크립트 변경
    싱글턴 전환 후 여러 씬에 대응 할 수 있게 변경

--- Assets/02_Scripts/Scene/SceneChanger.cs
+++ Assets/02_Scripts/Scene/SceneChanger.cs
@@ -1,10 +1,53 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneChanger : MonoBehaviour
+public enum SceneType
 {
-    public void ChangeScene()
+    TITLE,
+    AM,
+    PM,
+    BATTLE,
+}
+
+public class SceneChanger
+{
+    static SceneChanger instance;
+
+    public static SceneChanger Instance
     {
-        SceneManager.LoadScene("model_test");
+        get
+        {
+            if (instance == null)
+            {
+                instance = new SceneChanger();
+            }
+            return instance;
+        }
+    }
+
+    public void ChangeScene(SceneType sceneType)
+    {
+        switch (sceneType)
+        {
+            case SceneType.TITLE:
+                {
+                    SceneManager.LoadScene("TITLE_PT_3");
+                    break;
+                }
+            case SceneType.AM:
+                {
+                    SceneManager.LoadScene("AM_PT_3");
+                    break;
+                }
+            case SceneType.PM:
+                {
+                    break;
+                }
+            case SceneType.BATTLE:
+                {
+                    SceneManager.LoadScene("Battle_PT_3");                    
+                    break;
+                }
+        }        
     }
 }
