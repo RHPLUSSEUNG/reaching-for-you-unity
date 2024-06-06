1dd8f90e6933fa5ad18906a205747f88706e5aba
REofCC <buzz0131@naver.com>
2024-06-06 20:54:34 +0900

    feat: CameraMode 변경 기능 추가

--- Assets/02_Scripts/Etc/CameraController.cs
+++ Assets/02_Scripts/Etc/CameraController.cs
@@ -1,14 +1,15 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     [SerializeField]
     GameObject cameraList;
-    [SerializeField]
-    GameObject followList;
 
     Transform[] cameraTargets;
-    GameObject[] followTargets;
+    [SerializeField]
+    List<GameObject> followTargets;
     [SerializeField]
     float offsetX = 0.0f;
     [SerializeField]
@@ -36,11 +37,6 @@ public class CameraController : MonoBehaviour
             cameraTargets = cameraList.GetComponentsInChildren<Transform>();
             cameraIndex = 1;     // GetComponentsInChildren 사용 시 0번엔 부모 오브젝트 정보가 위치함으로 Index를 1부터
         }
-        if (followList != null)
-        {
-            followTargets = followList.GetComponentsInChildren<GameObject>();
-            followIndex = 0;
-        }
 
         if (isFollowMode)
         {
@@ -51,10 +47,6 @@ public class CameraController : MonoBehaviour
         {
             ChangeCameraTarget(cameraIndex, false);
         }
-
-        // 임시 캐릭터 따라가기 설정 코드
-        followTargets = GameObject.FindGameObjectsWithTag("Player");
-        followIndex= 0;
     }
     void FixedUpdate()
     {
@@ -127,21 +119,27 @@ public class CameraController : MonoBehaviour
         isFollowMode = true;
         Debug.Log("Changed FollowTarget To " + targetIndex);
     }
-    public void ChangeCameraMode(bool _isSmoothMove)
+    public void ChangeCameraMode(CameraMode mode, bool _isSmoothMove)
     {
-        if (isFollowMode)
+        switch(mode)
         {
-            isFollowMode = false;
-            targetTransform = cameraTargets[cameraIndex].transform;
-
-        }
-        else
-        {
-           isFollowMode = true;
-            targetTransform = followTargets[followIndex].transform;
+            case CameraMode.Static:
+                isFollowMode = false;
+                targetTransform = cameraTargets[cameraIndex].transform;
+                break;
+            case CameraMode.Follow:
+                isFollowMode = true;
+                transform.eulerAngles = new Vector3(rotateX, 0, 0);
+                targetTransform = followTargets[followIndex].transform;
+                break;
         }
         isSmoothMove = _isSmoothMove;
         Debug.Log("Changed Mode");
+    }
+
+    public void AddFollowList(GameObject target)
+    {
+        followTargets.Add(target);
     }
     public void ChangePos(Transform target)
     {
