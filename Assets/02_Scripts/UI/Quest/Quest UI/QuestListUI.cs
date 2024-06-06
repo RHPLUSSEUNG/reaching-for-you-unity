b64b1cf491a8ea1446b3571d68a92270e92bb318
이승형 <102710821+RHPLUSSEUNG@users.noreply.github.com>
2024-06-06 21:16:24 +0900

    feat: Quest 분류 기능 구현
    메인 퀘스트와 서브 퀘스트 구분

--- Assets/02_Scripts/UI/Quest/Quest UI/QuestListUI.cs
+++ Assets/02_Scripts/UI/Quest/Quest UI/QuestListUI.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class QuestListUI : MonoBehaviour
-{    
+{
+    [SerializeField] Transform mainQuestContent;
+    [SerializeField] Transform subQuestContent;
+    [SerializeField] Transform completedQuestContent;
     [SerializeField] QuestItemUI questPrefab;
+    [SerializeField] GameObject emptyText;
     QuestList questList;
 
     private void Start()
@@ -15,15 +19,48 @@ public class QuestListUI : MonoBehaviour
     }
 
     private void Redraw()
-    {
-        foreach (Transform item in transform)
+    {        
+        foreach (Transform item in mainQuestContent)
+        {            
+            Destroy(item.gameObject);
+        }
+        foreach (Transform item in subQuestContent)
+        {
+            Destroy(item.gameObject);
+        }
+        foreach (Transform item in completedQuestContent)
         {
             Destroy(item.gameObject);
         }
 
         foreach (QuestStatus status in questList.GetStatuses())
         {
-            QuestItemUI uiInstance = Instantiate<QuestItemUI>(questPrefab, transform);
+            QuestItemUI uiInstance;
+
+            switch (status.GetQuest().GetQuestType())
+            {
+                case QuestType.MAIN_QUEST:
+                    {
+                        uiInstance = Instantiate<QuestItemUI>(questPrefab, mainQuestContent);
+                        break;
+                    }
+                case QuestType.SUB_QUEST:
+                    {
+                         uiInstance = Instantiate<QuestItemUI>(questPrefab, subQuestContent);
+                        break;
+                    }
+                case QuestType.COMPLETED_QUEST:
+                    {
+                        uiInstance = Instantiate<QuestItemUI>(questPrefab, completedQuestContent);
+                        break;
+                    }
+                default:
+                    {
+                        uiInstance = null;
+                        break;
+                    }
+            }
+            
             uiInstance.Setup(status);
         }
     }
