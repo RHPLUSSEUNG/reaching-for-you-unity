7be3874ba31d510239be5a047956e4f4b330fae4
bye-on <codnjs842@naver.com>
2024-06-06 21:03:59 +0900

    chore: 기믹 정보 수정

--- Assets/02_Scripts/Cover/CoverBattle.cs
+++ Assets/02_Scripts/Cover/CoverBattle.cs
@@ -6,16 +6,11 @@ public class CoverBattle : MonoBehaviour
 {
     public CreateObject createObject;
 
-    private List<CoverData> covers = new List<CoverData>();
-
     void Start() {
-        UpdatePlayerPosition(transform.position); // 시작 시 플레이어 위치 설정
-        // 플레이어 위치로 바꿔야 됨
+        // UpdatePlayerPosition(transform.position); // 시작 시 플레이어 위치 설정
 
-        for(int i = 0; i < covers.Count; i++) 
-        {
-            Debug.Log(covers[i]);
-        }
+        Debug.Log(gameObject.transform.position);
+        
     }
 
     private void Update() {
@@ -24,22 +19,20 @@ public class CoverBattle : MonoBehaviour
 
     public void UpdatePlayerPosition(Vector3 newPosition) // 현재 위치 갱신
     {
-        Map[,] map = createObject.GetMap();
-        for (int x = 0; x < createObject.Width; x++)
-        {
-            for (int z = 0; z < createObject.Height; z++)
-            {
-                if (map[x, z].eObstacle == EObstacle.Obstacle)
-                {
-                    CoverData coverData = GetCoverDataAtPosition(map[x, z].ObjectLocation);
-                    covers.Add(coverData);
-                    if (coverData != null)
-                    {
-                        coverData.UpdatePlayerPosition(newPosition);
-                    }
-                }
-            }
-        }
+        // for (int x = 0; x < createObject.Width; x++)
+        // {
+        //     for (int z = 0; z < createObject.Height; z++)
+        //     {
+        //         if (map[x, z].eObstacle == EObstacle.Obstacle)
+        //         {
+        //             CoverData coverData = GetCoverDataAtPosition(map[x, z].ObjectLocation);
+        //             if (coverData != null)
+        //             {
+        //                 coverData.UpdatePlayerPosition(newPosition);
+        //             }
+        //         }
+        //     }
+        // }
     }
 
     void CheckTarget(Vector3 targetPosition) {
