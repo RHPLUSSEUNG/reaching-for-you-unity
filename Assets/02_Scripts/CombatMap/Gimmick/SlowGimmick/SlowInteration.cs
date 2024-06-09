8e9dfe6e4acb48b96e8764a1e17fa82d685557cd
bye-on <codnjs842@naver.com>
2024-06-09 11:08:44 +0900

    feat: 이동력 감소 기믹 추가

--- Assets/02_Scripts/CombatMap/Gimmick/SlowGimmick/SlowInteration.cs
+++ Assets/02_Scripts/CombatMap/Gimmick/SlowGimmick/SlowInteration.cs
@@ -1,58 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SlowInteration : MonoBehaviour
 {
-    [Header("FallingPrefabs")]
-    public GameObject[] FallingPrefabs;
-    public Transform FallingObjectTransform;
+    GameObject character;
+    CharacterState characterstate;
+    EntityStat characterstat;
 
-    private Rigidbody faliingRigid;
+    Text warningText;
+    Slow slow;
 
     int gimmickTurnCnt = 2; // 기믹 작동에 필요한 턴 개수
+    int remainTurnCnt = -1;
     int currentTurnCnt = -1; // 현재 총 턴 횟수
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        int randomIndex = Random.Range(0, FallingPrefabs.Length);
-        
-        // 초기화 작업
-        GameObject fallObject = Instantiate(FallingPrefabs[randomIndex], FallingObjectTransform);
-        fallObject.transform.localPosition = Vector3.zero;
-
-        FallingObjectTransform.gameObject.SetActive(false);
-        
-        faliingRigid = FallingObjectTransform.GetComponent<Rigidbody>();
-        if (faliingRigid == null)
-            faliingRigid = fallObject.AddComponent<Rigidbody>();
-
-        // 물리 작용 끔
-        faliingRigid.isKinematic = true;
-    }
 
     private void OnTriggerEnter(Collider other) 
     {
-        if (other.CompareTag("Player")) 
+        if (Managers.Battle.currentCharacter != null && Managers.Battle.currentCharacter != character)
         {
-            Debug.Log("Enter the Gimmick!!");
+            character = Managers.raycast.character;
+            characterstat = Managers.raycast.characterstat;
+            characterstate = Managers.raycast.characterstate;
+        }
 
+        if (Managers.Battle.battleState == BattleState.PlayerTurn)
+        {
+            slow = new();
+            slow.SetDebuff(gimmickTurnCnt, character, 10, true);
+
+            warningText = Managers.BattleUI.warningUI.GetText();
             currentTurnCnt = Managers.Battle.totalTurnCnt;
-            Debug.Log("현재 " + gimmickTurnCnt + " 이후 기믹 발동");
+
+            StartCoroutine(ShowWarningCoroutine());
+            Managers.BattleUI.warningUI.ShowWarningUI();  
         }
     }
 
-    private void OnTriggerStay(Collider other) {
-        if (other.CompareTag("Player")) 
-        {
-            int remainTurnCnt = Managers.Battle.totalTurnCnt - currentTurnCnt;
-
-            if(remainTurnCnt > gimmickTurnCnt) 
-            {
-                FallingObjectTransform.gameObject.SetActive(true);
-                faliingRigid.isKinematic = false;
-            }
-        }
+    private IEnumerator ShowWarningCoroutine()
+    {
+        warningText.text = $"{gimmickTurnCnt}턴동안 이동력 감소!!";
+        Managers.BattleUI.warningUI.SetText(warningText.text);
+        yield return new WaitForSeconds(2f);
+        Managers.Battle.isPlayerTurn = false;
     }
 }
