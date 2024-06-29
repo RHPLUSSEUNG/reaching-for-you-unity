78d12380c91bec74e6fdbc186f1f92f6134d3452
bye-on <codnjs842@naver.com>
2024-06-29 22:25:46 +0900

    feat: adventrue map generator

--- Assets/02_Scripts/AdventureMap/RandomPassage.cs
+++ Assets/02_Scripts/AdventureMap/RandomPassage.cs
@@ -4,36 +4,49 @@ using UnityEngine;
 
 public class RandomPassage : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject[] wallPrefabs;
     List<Transform> passageChildren = new List<Transform>();
     int activeCount = 0;
+
     void Start()
     {
-        Transform passage = transform.Find("Passage");
-
-        if (passage != null)
+        foreach (Transform t in transform)
         {
-            foreach (Transform child in passage)
-            {
-                passageChildren.Add(child);
-            }
+            int randomIndex = Random.Range(0, wallPrefabs.Length);
+            GameObject wallInMap = Instantiate(wallPrefabs[randomIndex], t);
+            wallInMap.transform.SetParent(t);
 
-            foreach (Transform child in passageChildren)
-            {
-                bool activate = Random.Range(0, 2) == 0;
-
-                child.gameObject.SetActive(activate);
-                if(!child.gameObject.activeSelf) activeCount++; // 비활성화 체크
-            }
-        }
-        else
-        {
-            Debug.LogError("Passage object not found!");
+            passageChildren.Add(t);
         }
 
-        if(activeCount == 4)
-        {
-            int randomIndex = Random.Range(0, passageChildren.Count);
-            passageChildren[randomIndex].gameObject.SetActive(true);
-        }
+
+        // Transform passage = transform.Find("Passage");
+
+        // if (passage != null)
+        // {
+        //     foreach (Transform child in passage)
+        //     {
+        //         passageChildren.Add(child);
+        //     }
+
+        //     foreach (Transform child in passageChildren)
+        //     {
+        //         bool activate = Random.Range(0, 2) == 0;
+
+        //         child.gameObject.SetActive(activate);
+        //         if(!child.gameObject.activeSelf) activeCount++; // 비활성화 체크
+        //     }
+        // }
+        // else
+        // {
+        //     Debug.LogError("Passage object not found!");
+        // }
+
+        // if(activeCount == 4)
+        // {
+        //     int randomIndex = Random.Range(0, passageChildren.Count);
+        //     passageChildren[randomIndex].gameObject.SetActive(true);
+        // }
     }
 }
