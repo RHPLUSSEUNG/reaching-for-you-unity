d29e3b6953d029cfbe8f934737331eb0f82fc454
이승형 <102710821+RHPLUSSEUNG@users.noreply.github.com>
2024-07-15 17:51:25 +0900

    feat: 설정 창 상단 탭 버튼 기능
    버튼을 누르면, 일반 - 조작 사이 스크롤 기능 구현

---
+++ Assets/02_Scripts/UI/Title/Setting/SettingUI.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingUI : MonoBehaviour
+{
+    [Header("Top Button Panel")]
+    [SerializeField] Button generalButton;
+    [SerializeField] Button controlButton;
+    [SerializeField] Scrollbar scrollbar;
+    
+    float scrollSpeed = 10f;
+    Coroutine currentCoroutine;
+
+    private void Start()
+    {
+        generalButton.onClick.AddListener(() => TopButtonClick(true));
+        controlButton.onClick.AddListener(() => TopButtonClick(false));
+    }
+
+    void TopButtonClick(bool _isGeneral)
+    {        
+        if(currentCoroutine != null)
+        {
+            StopCoroutine(currentCoroutine);
+        }
+
+        currentCoroutine = StartCoroutine(SlideSettingWindow(_isGeneral));
+    }
+
+    IEnumerator SlideSettingWindow(bool _isGeneral)
+    {
+        while(true)
+        {
+            yield return null;
+
+            if (_isGeneral)
+            {
+                scrollbar.value = Mathf.Lerp(scrollbar.value, 0f, Time.deltaTime * scrollSpeed);
+
+                if (Mathf.Approximately(scrollbar.value, 0f))
+                {
+                    scrollbar.value = 0f;
+                    yield break;
+                }
+            }
+            else
+            {
+                scrollbar.value = Mathf.Lerp(scrollbar.value, 0.5f, Time.deltaTime * 10f);
+
+                if (Mathf.Approximately(scrollbar.value, 0.5f))
+                {
+                    scrollbar.value = 0.5f;
+                    yield break;
+                }
+            }
+        }
+                       
+    }
+}
