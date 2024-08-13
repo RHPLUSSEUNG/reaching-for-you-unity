461e708566ffad53dc0b8f46c0144960a5eedbec
이승형 <102710821+RHPLUSSEUNG@users.noreply.github.com>
2024-08-13 16:06:16 +0900

    feat: KeyBind 버튼과 연동
    바뀐 키로 표시

--- Assets/02_Scripts/UI/Title/Keybind/KeyBindManager.cs
+++ Assets/02_Scripts/UI/Title/Keybind/KeyBindManager.cs
@@ -20,30 +20,19 @@ public static class KeySetting
 }
 
 public class KeyBindManager : MonoBehaviour
-{
-    [SerializeField] List<Button> bindButtons = new List<Button>();
-
+{    
     int key = -1;
     KeyCode[] defaultKeys = new KeyCode[] { KeyCode.W, KeyCode.S, KeyCode.A, KeyCode.D, KeyCode.E, KeyCode.Tab };
 
     void Awake()
-    {
-        for(int i = 0; i < bindButtons.Count; i++)
-        {
-            bindButtons[i].onClick.AddListener(() => BindKey(i));
-        }
-    }
-
-    void Start()
-    {
+    {       
         //[USE] Input.GetKey(KeySetting.keys[KeyType.ACTION]);
-
-        for(int i = 0; i < (int)KeyType.MAX_NUM; i++)
+        for (int i = 0; i < (int)KeyType.MAX_NUM; i++)
         {
             KeySetting.keys.Add((KeyType)i, defaultKeys[i]);
         }
     }
-
+    
     void OnGUI()
     {
         Event keyEvent = Event.current;
