103df0f4238a7c8634a170ba91fa3eb56cdaf9bc
이승형 <102710821+RHPLUSSEUNG@users.noreply.github.com>
2024-09-30 17:22:57 +0900

    feat: 맵 선택 UI 스크립트 싱글턴 변경
    조작의 용이를 위해 싱글턴 패턴으로 변경

--- Assets/02_Scripts/UI/Adventure Map Select UI/AdventureMapSelectUI.cs
+++ Assets/02_Scripts/UI/Adventure Map Select UI/AdventureMapSelectUI.cs
@@ -5,21 +5,47 @@ using UnityEngine.UI;
 
 public class AdventureMapSelectUI : MonoBehaviour
 {
+    public static AdventureMapSelectUI Instance;
     [SerializeField] Transform selectMapPanel;
     [SerializeField] List<Button> selectButton = new List<Button>();
+    [SerializeField] Button exitButton;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
 
     private void Start()
     {
+        exitButton.onClick.AddListener(ClosePanel);
         for (int i = 0; i < selectButton.Count; i++)
         {
             int index = i;
             selectButton[i].onClick.AddListener(() => SelectMap(index));
         }
+        ClosePanel();
     }
 
     private void SelectMap(int index)
     {
         AdventureManager.StageNumber = index;
         SceneChanger.Instance.ChangeScene(SceneType.PM_ADVENTURE);
+    }
+
+    public void OpenPanel()
+    {
+        selectMapPanel.gameObject.SetActive(true);
+    }
+
+    public void ClosePanel()
+    {
+        selectMapPanel.gameObject.SetActive(false);
     }
 }
