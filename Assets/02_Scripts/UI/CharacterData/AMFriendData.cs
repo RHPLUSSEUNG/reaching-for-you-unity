154a6165052ab02a1f6392f8df7b280a09192ac8
이승형 <102710821+RHPLUSSEUNG@users.noreply.github.com>
2024-10-10 14:56:33 +0900

    feat: CharacterData 수정
    다른 스크립트에서 쓸 수 있도록 수정

---
+++ Assets/02_Scripts/UI/CharacterData/AMFriendData.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Friend", menuName = "Character/Friend")]
+public class AMFriendData : AMCharacterData
+{
+    [SerializeField] int friendship = 0;
+    [SerializeField] GameObject friendshipEffect;
+
+    void Start()
+    {
+        friendshipEffect.SetActive(false);
+    }
+
+    public void RiseFriendshipLevel(int level)
+    {
+        if (friendship + level >= 6)
+        {
+            friendship = 6;
+        }
+        else
+        {
+            friendship += level;
+            ActiveFriendshipEffect();
+        }
+    }
+
+    public void FallFriendshipLevel(int level)
+    {
+        if (friendship - level <= 0)
+        {
+            friendship = 0;
+        }
+        else
+        {
+            friendship -= level;
+        }
+    }
+
+    public int GetFriendship()
+    {
+        return friendship;
+    }
+
+    void ActiveFriendshipEffect()
+    {
+        friendshipEffect.SetActive(true);
+    }
+}
