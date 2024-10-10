3a0b6bf50fe5c1663583f66f1584a0a02b6f550f
REofCC <buzz0131@naver.com>
2024-10-10 16:44:58 +0900

    feat : 랜덤 위치 폭발 생성 및 패러미터 설정

---
+++ Assets/02_Scripts/Minigame/BattleGuide/BattleGuideManager.cs
@@ -0,0 +1,170 @@
+
+using System.Collections;
+using UnityEngine;
+
+public class BattleGuideManager : MonoBehaviour
+{
+    public float currentTime;
+    public float score;
+    private bool isPlaying;
+    private float hitTimer;
+    private float spawnTimer;
+    private float randSpwanTime;
+    private bool canHit;
+    private bool canSpawnTime;
+
+    protected int currentHitZone;
+
+    [Header("Game Setting")]
+    [SerializeField]
+    public float maxTime;
+    [SerializeField]
+    GameObject player;
+    [SerializeField]
+    protected int life;
+    [SerializeField]
+    protected float hitInterval;
+    [SerializeField]
+    GameObject plane;
+    [SerializeField]
+    GameObject hitZone;
+
+    BoxCollider boxCollider;
+
+
+    [Header("Phase 1")]
+    [SerializeField]
+    protected int maxSpawnCount;
+    [SerializeField]
+    [Range(0f, 3f)]
+    protected float minSpwanInterval;
+    [SerializeField]
+    [Range(0f, 3f)]
+    protected float maxSpwanInterval;
+    [SerializeField]
+    [Range(0f, 1f)]
+    protected float minRadius;
+    [SerializeField]
+    [Range(0f, 1f)]
+    protected float maxRadius;
+
+
+    private void Awake()
+    {
+        hitTimer = 0;
+        currentTime = 0;
+        life = 3;
+        maxTime = 100;
+        currentHitZone = 0;
+        isPlaying = true;
+        boxCollider = plane.GetComponent<BoxCollider>();
+    }
+    private void Start()
+    {
+        spawnHitZone();
+        StartCoroutine(spawnHitZoneCoroutine());
+    }
+
+    public void FixedUpdate()
+    {
+        if (isPlaying)
+        {
+            currentTime += Time.deltaTime;
+            if (currentTime >= maxTime)
+            {
+                isPlaying = false;
+                GameOver();
+            }
+        }
+    }
+    public void spawnHitZone()
+    {
+        currentHitZone++;
+        canSpawnTime = false;
+        spawnTimer = 0;
+        Instantiate(hitZone, GetRandPosition(), Quaternion.Euler(90, 0, 0));
+        randSpwanTime = GetRandSpawnTime();
+    }
+    public Vector3 GetRandPosition()
+    { 
+        Vector3 center = plane.transform.position;
+        float width = boxCollider.bounds.size.x;
+        float height = boxCollider.bounds.size.z;
+
+        float randX = Random.Range(-width / 2f, width / 2f);
+        float randZ = Random.Range(-height / 2f, height / 2f);
+
+        float y = center.y+0.1f;
+
+        Vector3 randPoint = new Vector3(center.x + randX, y, center.z + randZ);
+
+        return randPoint;
+    }
+
+    float GetRandSpawnTime()
+    {
+        return Random.Range(minSpwanInterval, maxSpwanInterval);
+    }
+    public void Hit()
+    {
+        if (!canHit)
+            return;
+
+        life--;
+        HitTimeCheck();
+        if (life <= 0 )
+        {
+            isPlaying = false;
+            GameOver();
+        }
+    }
+
+    public void GameOver()
+    {
+        StopCoroutine(spawnHitZoneCoroutine());
+        score = currentTime * life;
+    }
+    void HitTimeCheck()
+    {
+        canHit = false;
+        hitTimer = 0;
+        StartCoroutine(HitTimer());
+    }
+
+    IEnumerator HitTimer()
+    {
+        //플레이어 무적 이펙트
+        hitTimer += Time.deltaTime;
+        if( hitTimer >= hitInterval) 
+        { 
+            canHit = true;
+            yield break;
+        }
+        yield return null;
+    }
+    IEnumerator spawnHitZoneCoroutine()
+    {
+        while (!canSpawn())
+        {
+            spawnTimer += Time.deltaTime;
+            if (spawnTimer >= randSpwanTime)
+                canSpawnTime = true;
+
+            yield return null;
+        }
+
+        spawnHitZone();
+        StartCoroutine(spawnHitZoneCoroutine());
+    }
+    bool canSpawn()
+    {
+        if (currentHitZone >= maxSpawnCount || !canSpawnTime)
+            return false;
+        else
+            return true;
+    }
+    public void HitZoneDestroy()
+    {
+        currentHitZone--;
+    }
+}
