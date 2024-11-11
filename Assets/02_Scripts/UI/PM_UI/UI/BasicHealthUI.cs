183aeaa3b429803ddaa68db8a0d156b67cf18a52
LeeJiHun <102594788+JiHoon-Lee-Tony@users.noreply.github.com>
2024-11-11 14:47:32 +0900

    feat/캐릭터 영역에 닿았는지 검사하는 기능 추가

--- Assets/02_Scripts/UI/PM_UI/UI/BasicHealthUI.cs
+++ Assets/02_Scripts/UI/PM_UI/UI/BasicHealthUI.cs
@@ -1,159 +1,203 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Rendering;
-using UnityEngine.UI;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.Rendering;
+    using UnityEngine.UI;
 
-public class BasicHealthUI : UI_Scene
-{
-    enum basicHealthUI
+    public class BasicHealthUI : UI_Scene
     {
-        TimeLimit,
-        Character,
-        TimingBar,
-        TrueArea
-    }
-
-    [SerializeField]
-    Slider timeLimit;
-    [SerializeField]
-    Scrollbar _scrollbar;
-    [SerializeField]
-    RectTransform _trueAreaRect;
-    RectTransform _scrollRect;
-
-    float duration = 2.0f;
-    float totalTime = 60f;
-
-    float trueAreaMin = 200f;
-    float trueAreaMax = 300f;
-    bool isIncreasing = true;
-
-    public override void Init()
-    {
-        Bind<GameObject>(typeof(basicHealthUI));
-
-        timeLimit = GetObject((int)basicHealthUI.TimeLimit).GetComponent<Slider>();
-        _scrollbar = GetObject((int)basicHealthUI.TimingBar).GetComponent<Scrollbar>();
-        _scrollRect = _scrollbar.GetComponent<RectTransform>();
-        _trueAreaRect = GetObject((int)basicHealthUI.TrueArea).GetComponent<RectTransform>();
-
-        SetTrueArea();
-        StartCoroutine(MiniGameStart());
-    }
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.E))
+        enum basicHealthUI
         {
-            bool check = CheckTrueArea(_scrollbar.value);
-            if(check)
+            TimeLimit,
+            LeftProhibitedArea,
+            RightProhibitedArea,
+            Character,
+            TimingBar,
+            TrueArea
+        }
+
+        [SerializeField]
+        Slider timeLimit;
+        [SerializeField]
+        Scrollbar _scrollbar;
+        [SerializeField]
+        RectTransform _trueAreaRect;
+        RectTransform _scrollRect;
+
+        RectTransform characterRectTrnasform;
+        RectTransform leftProhibit;
+        RectTransform rightProhibit;
+
+        float duration = 2.0f;
+        float totalTime = 60f;
+
+        float trueAreaMin = 200f;
+        float trueAreaMax = 300f;
+
+        float moveDistance = 300f;
+        bool isIncreasing = true;
+
+        public override void Init()
+        {
+            Bind<GameObject>(typeof(basicHealthUI));
+
+            timeLimit = GetObject((int)basicHealthUI.TimeLimit).GetComponent<Slider>();
+            _scrollbar = GetObject((int)basicHealthUI.TimingBar).GetComponent<Scrollbar>();
+            _scrollRect = _scrollbar.GetComponent<RectTransform>();
+            _trueAreaRect = GetObject((int)basicHealthUI.TrueArea).GetComponent<RectTransform>();
+
+            characterRectTrnasform = GetObject((int)basicHealthUI.Character).GetComponent<RectTransform>();
+            leftProhibit = GetObject((int)basicHealthUI.LeftProhibitedArea).GetComponent<RectTransform>();
+            rightProhibit = GetObject((int)basicHealthUI.RightProhibitedArea).GetComponent<RectTransform>();
+
+            SetTrueArea();
+            StartCoroutine(MiniGameStart());
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.E))
             {
-                SetTrueArea();
+                bool check = CheckTrueArea(_scrollbar.value);
+                if(check)
+                {
+                    SetTrueArea();
+                }
+            }
+
+            if(Input.GetKeyDown(KeyCode.C))
+            {
+                bool leftCheck = IsOverlapping(characterRectTrnasform, leftProhibit);
+                Debug.Log($"leftCheck : {leftCheck}");
+                bool rightCheck = IsOverlapping(characterRectTrnasform, rightProhibit);
+                Debug.Log($"rightCheck : {rightCheck}");
             }
         }
-    }
 
-    void SetTrueArea()
-    {
-        float fotRangeValue = Random.Range(10f, 90f);
-        float fotSize = Random.Range(trueAreaMin, trueAreaMax);
-
-        _trueAreaRect.gameObject.SetActive(true);
-        _trueAreaRect.anchoredPosition = new Vector2(Mathf.Lerp(0, _scrollRect.sizeDelta.x, fotRangeValue / 100f), 0);
-        _trueAreaRect.sizeDelta = new Vector2(fotSize, _scrollRect.sizeDelta.y);
-    }
-
-    IEnumerator MiniGameStart()
-    {
-        float elapsed = 0f;
-
-        StartCoroutine(TimeLimitStart());
-        while (elapsed < totalTime)
+        void SetTrueArea()
         {
-            float timer = 0f;
+            float fotRangeValue = Random.Range(10f, 90f);
+            float fotSize = Random.Range(trueAreaMin, trueAreaMax);
 
-            if (isIncreasing)
+            _trueAreaRect.gameObject.SetActive(true);
+            _trueAreaRect.anchoredPosition = new Vector2(Mathf.Lerp(0, _scrollRect.sizeDelta.x, fotRangeValue / 100f), 0);
+            _trueAreaRect.sizeDelta = new Vector2(fotSize, _scrollRect.sizeDelta.y);
+        }
+
+        IEnumerator MiniGameStart()
+        {
+            float elapsed = 0f;
+
+            StartCoroutine(TimeLimitStart());
+            while (elapsed < totalTime)
             {
-                while (timer < duration)
+                float timer = 0f;
+
+                if (isIncreasing)
                 {
-                    _scrollbar.value = Mathf.Lerp(0, 1, timer / duration);
-                    timer += Time.deltaTime;
-                    elapsed += Time.deltaTime;
-                    yield return null;
+                    while (timer < duration)
+                    {
+                        _scrollbar.value = Mathf.Lerp(0, 1, timer / duration);
+                        timer += Time.deltaTime;
+                        elapsed += Time.deltaTime;
+                        yield return null;
+                    }
                 }
+                else
+                {
+                    while (timer < duration)
+                    {
+                        _scrollbar.value = Mathf.Lerp(1, 0, timer / duration);
+                        timer += Time.deltaTime;
+                        elapsed += Time.deltaTime;
+                        yield return null;
+                    }
+                }
+
+                isIncreasing = !isIncreasing;
+            }
+        }
+
+        IEnumerator TimeLimitStart()
+        {
+            float elapsed = 0f;
+
+            while (elapsed < totalTime)
+            {
+                timeLimit.value = Mathf.Lerp(1, 0, elapsed / totalTime);
+                elapsed += Time.deltaTime;
+                yield return null;
+            }
+        }
+
+        bool CheckTrueArea(float value)
+        {
+            RectTransform slidingArea = _scrollbar.GetComponent<RectTransform>();
+            float slidingAreaWidth = slidingArea.rect.width;
+
+            float trueAreaPos = _trueAreaRect.anchoredPosition.x;
+
+            float trueAreaStart = trueAreaPos - (_trueAreaRect.rect.width * _trueAreaRect.pivot.x);
+            float trueAreaEnd = trueAreaStart + _trueAreaRect.rect.width;
+
+            float imageStartValue = Mathf.Clamp01(trueAreaStart / slidingAreaWidth);
+            float imageEndValue = Mathf.Clamp01(trueAreaEnd / slidingAreaWidth);
+
+            if(value >= imageStartValue && value <= imageEndValue)
+            {
+                Debug.Log("MiniGame Success");
+                SuccessTiming();
+                return true;
             }
             else
             {
-                while (timer < duration)
-                {
-                    _scrollbar.value = Mathf.Lerp(1, 0, timer / duration);
-                    timer += Time.deltaTime;
-                    elapsed += Time.deltaTime;
-                    yield return null;
-                }
+                Debug.Log("MiniGame Fail");
+                FailTiming();
+                return false;
             }
-
-            isIncreasing = !isIncreasing;
         }
-    }
 
-    IEnumerator TimeLimitStart()
-    {
-        float elapsed = 0f;
-
-        while (elapsed < totalTime)
+        void SuccessTiming()
         {
-            timeLimit.value = Mathf.Lerp(1, 0, elapsed / totalTime);
-            elapsed += Time.deltaTime;
-            yield return null;
+            Vector2 newPos = characterRectTrnasform.anchoredPosition;
+            newPos.x += moveDistance;
+            if(newPos.x >= rightProhibit.position.x)
+            {
+                newPos.x = rightProhibit.position.x;
+            }
+            characterRectTrnasform.anchoredPosition = newPos;
         }
-    }
 
-    bool CheckTrueArea(float value)
-    {
-        RectTransform slidingArea = _scrollbar.GetComponent<RectTransform>();
-        float slidingAreaWidth = slidingArea.rect.width;
-
-        float trueAreaLocalX = _trueAreaRect.anchoredPosition.x;
-
-        float trueAreaStartX = trueAreaLocalX - (_trueAreaRect.rect.width * _trueAreaRect.pivot.x);
-        float trueAreaEndX = trueAreaStartX + _trueAreaRect.rect.width;
-
-        float imageStartValue = Mathf.Clamp01(trueAreaStartX / slidingAreaWidth);
-        float imageEndValue = Mathf.Clamp01(trueAreaEndX / slidingAreaWidth);
-
-        if(value >= imageStartValue && value <= imageEndValue)
+        void FailTiming()
         {
-            Debug.Log("Success");
-            SuccessTiming();
-            return true;
+            Vector2 newPos = characterRectTrnasform.anchoredPosition;
+            newPos.x -= moveDistance;
+            if(newPos.x <= leftProhibit.position.x)
+            {
+                newPos.x = leftProhibit.position.x;
+            }
+            characterRectTrnasform.anchoredPosition = newPos;
         }
-        else
+
+        bool IsOverlapping(RectTransform character, RectTransform prohibitArea)
         {
-            Debug.Log("Fail");
-            FailTiming();
-            return false;
+            Rect characterRect = GetWorldRect(character);
+            Rect prohibitRect = GetWorldRect(prohibitArea);
+
+            return characterRect.Overlaps(prohibitRect);
+        }
+
+        Rect GetWorldRect(RectTransform rect)
+        {
+            Vector3[] corners = new Vector3[4];
+            rect.GetWorldCorners(corners);
+
+            float xMin = corners[0].x;
+            float xMax = corners[2].x;
+            float yMin = corners[0].y;
+            float yMax = corners[2].y;
+
+            return new Rect(xMin, yMin, xMax - xMin, yMax - yMin);
+
         }
     }
-
-    void SuccessTiming()
-    {
-        GameObject character = GetObject((int)basicHealthUI.Character);
-        RectTransform charcterTransform = character.GetComponent<RectTransform>();
-
-        Vector2 newPos = charcterTransform.anchoredPosition;
-        newPos.x += 200;
-        charcterTransform.anchoredPosition = newPos;
-    }
-
-    void FailTiming()
-    {
-        GameObject character = GetObject((int)basicHealthUI.Character);
-        RectTransform charcterTransform = character.GetComponent<RectTransform>();
-
-        Vector2 newPos = charcterTransform.anchoredPosition;
-        newPos.x -= 200;
-        charcterTransform.anchoredPosition = newPos;
-    }
-}
